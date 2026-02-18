0474a12198ccd255dff754d27e0b145e5e99df74
Daniel Neamati <danineamati@gmail.com>
2026-02-17 17:12:32 -0800

    Squashed the sorting bugs in concat

--- src/concat_data_script.py
+++ src/concat_data_script.py
@@ -14,38 +14,29 @@ import sys             # For system arguments
 from concat_data_utils import concat_logger_data
 
 if __name__ == "__main__":
-    # Concatenate the data for each logger
-    # concat_logger_data(
-    #     "ca0c",
-    #     "data/burnbot-post-burn-rohan/",
-    #     "data/burnbot-post-burn/"
-    # )
-    # concat_logger_data(
-    #     logger_name="62e4",
-    #     data_dir="data/burnbot-post-burn-62e4/",
-    #     output_dir="data/burnbot-post-burn/"
-    # )
+    # Concatenate the data for each logger. For example,
     # concat_logger_data(
     #     logger_name="0ff4",
-    #     data_dir="raw_data/data_2025-02-09/data/",
-    #     output_dir="data/winter-term-packard/"
+    #     raw_data_dir="raw_data/data_2025-02-09/data/",
+    #     output_data_dir="data/winter-term-packard/"
     # )
 
     # Set default values for easier VS Code debugging
     logger_name = "f864"
-    data_dir = pathlib.Path(
+    raw_data_dir = pathlib.Path(
         r"raw_data/pepperwood_2025-10-23_19-54")
-    output_dir = pathlib.Path("data/pepperwood_2025_gdrive/pepperwood_2025_gdrive_10-23_19-54")
+    output_data_dir = pathlib.Path(
+        "data/pepperwood_2025_gdrive/pepperwood_2025_gdrive_10-23_19-54")
 
     if len(sys.argv) > 1:
         logger_name = sys.argv[1]
         # The path may be a windows path, so we need to treat it as a raw
         # string to avoid escape characters
-        data_dir = pathlib.Path(r"{}".format(sys.argv[2]))
-        output_dir = pathlib.Path(r"{}".format(sys.argv[3]))
+        raw_data_dir = pathlib.Path(r"{}".format(sys.argv[2]))
+        output_data_dir = pathlib.Path(r"{}".format(sys.argv[3]))
 
     concat_logger_data(
         logger_name=logger_name,
-        data_dir=data_dir,
-        output_dir=output_dir
+        raw_data_dir=raw_data_dir,
+        output_data_dir=output_data_dir
     )
